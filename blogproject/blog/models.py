869a5edcb9bd3cc79849677b12c6131fa2fcee01
a58982284 <a58982284@163.com>
2018-04-30 09:22:56 +0800

    第三次

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -24,9 +24,9 @@ class Post(models.Model):
     excerpt = models.CharField(max_length=200,blank=True)
     created_time = models.DateTimeField()
     modified_time = models.DateTimeField()
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
